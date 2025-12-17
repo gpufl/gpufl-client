beb96ec80a8960b93f371ba6279aad35ee575ffd
Myoungho Shin <myounghoshin84@gmail.com>
2025-12-16 22:57:49 -0800

    simplify the metric by removing the sampling for kernel and scope

--- example/python/01_basic.py
+++ example/python/01_basic.py
@@ -1,25 +1,27 @@
-import gpufl as gfl
+from gpufl._gpufl_client import Scope, init, shutdown
 import time
 
 # 1. Initialize the library
 # Arguments: (AppName, LogFilePath, SampleIntervalMs)
 # Interval=0 means "Only log start/end", no background sampling.
-gfl.init("PythonDemo", "gpufl_basic", 5)
+res = init("PythonDemo", "gpufl_basic", 5)
+
+print(f"res = {res}")
 
 print("Starting Trace...")
 
 # 2. Define a Scope
 # This will write a "scope_begin" event immediately.
-with gfl.Scope("Initialization"):
+with Scope("Initialization"):
     print("  inside scope 'Initialization'")
     time.sleep(0.5)
 
 # 3. Define another Scope with a Tag
 # Tags are useful for filtering (e.g., "loading", "compute")
-with gfl.Scope("DataLoading", "io-bound"):
+with Scope("DataLoading", "io-bound"):
     print("  inside scope 'DataLoading'")
     time.sleep(0.2)
 
 # 4. Cleanup (Optional, but good practice)
-gfl.shutdown()
+shutdown()
 print("Trace finished. Check 'gpufl_basic'")
